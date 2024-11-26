a049f5db8ce743b398fe269fcfa1a3343c941fe9
mbmande <mbsmande@gmail.com>
2024-11-26 23:09:31 +0000

    printing the message

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * for printing message
+ * @param {String} msg The message to display.
+ * ==============
+ */
+
+const displayMessage = (msg) => {
+	  console.log(msg);
+};
+
+module.exports = displayMessage;
