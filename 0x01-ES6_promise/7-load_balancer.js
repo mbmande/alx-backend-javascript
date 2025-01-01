92a0083c813d5b1838783cb0c36874cd373ebee5
mbmande <mbsmande@gmail.com>
2025-01-01 19:01:44 +0000

    load commit

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,12 @@
+/**
+ *
+ *
+ * jfhifs
+ *
+ * sfshfs
+ */
+
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise.race([chinaDownload, USDownload])
+    .then((value) => value);
+}
