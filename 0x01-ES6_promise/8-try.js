554fba9e46f285baa0b5afd2d4b1f3435c330d41
mbmande <mbsmande@gmail.com>
2025-01-01 19:05:09 +0000

    try commit

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,13 @@
+/**
+ * ushifssff
+ * fhiih
+ * ufh/
+ *
+ */
+
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  }
+  return numerator / denominator;
+}
