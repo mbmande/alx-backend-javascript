9e0b34f26b087e8695bf3b70d5b03e2881510982
mbmande <mbsmande@gmail.com>
2025-01-01 05:57:40 +0000

    creating objects

---
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/**
+ *
+ *
+ * ushhdjsdh
+ * sndsjd
+ */
+
+export default function createEmployeesObject(departmentName, employees) {
+  return {
+    [`${departmentName}`]: employees,
+  };
+}
