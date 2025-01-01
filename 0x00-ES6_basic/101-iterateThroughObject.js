99f7ffffbd0e69c7259e8ae0b9e635ffc66fc8c9
mbmande <mbsmande@gmail.com>
2025-01-01 06:16:40 +0000

    through onject update

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,20 @@
+/**
+ *
+ *
+ *
+ * hfgfgf
+ * dhsdhsd
+ */
+
+export default function iterateThroughObject(reportWithIterator) {
+  let output = '';
+
+  for (const [index, item] of Object.entries(reportWithIterator)) {
+    output += `${item}`;
+
+    if (parseInt(index) !== reportWithIterator.length - 1) {
+      output += ' | ';
+    }
+  }
+  return output;
+}
