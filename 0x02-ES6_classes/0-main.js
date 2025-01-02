b7cd55c0063e80d3bf162031193eb648b75d1ddf
Muhammad Mande <mbsmande@gmail.com>
2025-01-02 12:23:14 +0100

    Delete 0x02-ES6_classes directory

--- 0x02-ES6_classes/0-main.js
+++
@@ -1,9 +0,0 @@
-import ClassRoom from "./0-classroom.js";
-
-const room = new ClassRoom(10);
-console.log(room._maxStudentsSize)
-import ClassRoom from "./0-classroom.js";
-
-const room = new ClassRoom(10);
-console.log(room._maxStudentsSize)
-
