b8dccd43f0bca14041ebaf382bf30482b6881b6c
mbmande <mbsmande@gmail.com>
2025-01-02 11:00:38 +0000

    CLASS ONE update

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * ijhihksjf
+ * ffdggfskge
+ * s
+ *
+ * ;ljllf
+ */
+
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
