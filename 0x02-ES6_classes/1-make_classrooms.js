b8bd446e19e08efab6a5f46264578fba3451f83b
mbmande <mbsmande@gmail.com>
2025-01-02 14:37:54 +0000

    classroom commit

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,13 @@
+import ClassRoom from './0-classroom';
+/**
+ * wdjwodjwd
+ * wwfjfls
+ *
+ *
+ * jsfjfslf
+ * 
+ */
+
+export default function initializeRooms() {
+  return [19, 20, 34].map((size) => new ClassRoom(size));
+}
