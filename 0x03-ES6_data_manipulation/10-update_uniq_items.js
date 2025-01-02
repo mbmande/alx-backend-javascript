29411011024388af61b10e0577f40fea3f11d4cc
mbmande <mbsmande@gmail.com>
2025-01-02 18:39:40 +0000

    updated items

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,20 @@
+/**
+ * jhhfjshf
+ * sfjshfjshf
+ * fsfjgsffs
+ * fsfgwff
+ */
+/* eslint-disable no-param-reassign */
+export default function updateUniqueItems(map) {
+  if (map instanceof Map) {
+    for (const [key, value] of map) {
+      if (value === 1) {
+        map.set(key, 100);
+      }
+    }
+
+    return map;
+  }
+
+  throw new Error('Cannot process');
+}
