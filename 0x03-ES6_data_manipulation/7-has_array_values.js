6ae4eb793b52a1129abcd0d334b52e19c49f1752
mbmande <mbsmande@gmail.com>
2025-01-02 18:32:32 +0000

    array values commit

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,16 @@
+/**
+ * jfhhjfs
+ * fjfhs
+ * sfkhfkh
+ */
+/* eslint-disable array-callback-return */
+export default function hasValuesFromArray(set, list) {
+  let verdict = true;
+  list.map((x) => {
+    if (!set.has(x)) {
+      verdict = false;
+    }
+  });
+
+  return verdict;
+}
