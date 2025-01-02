721d7362467c7b76d7c03e3c115e61cd953037b4
mbmande <mbsmande@gmail.com>
2025-01-02 18:09:07 +0000

    list of students

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,12 @@
+/**
+ * jhff
+ * dghs
+ *
+ * hfjsfs
+ */
+
+export default function getListStudentIds(students) {
+  if (!Array.isArray(students)) return [];
+
+  return students.map((students) => students.id);
+}
